880b96d2f6443d91531584e141f07253149ffcac
Dimitrios J. Vergados <djvergad@gmail.com>
2014-10-23 11:56:32 +0300

    Moved bunny params to config/rabiitmq.yml

--- config/initializers/bunny.rb
+++ config/initializers/bunny.rb
@@ -1,5 +1,11 @@
+require 'yaml'
+config = YAML.load_file('config/rabbitmq.yml')
+ 
+$bunny = Bunny.new(:host => config[Rails.env]["host"],
+        :vhost => config[Rails.env]["vhost"],
+        :user => config[Rails.env]["user"],
+        :password => config[Rails.env]["password"],
+)
 
-#$bunny = Bunny.new("amqp://***REMOVED***:***REMOVED***@spotted-monkey.rmq.cloudamqp.com/***REMOVED***") 
-$bunny = Bunny.new
 $bunny.start
 $bunny_channel = $bunny.create_channel
