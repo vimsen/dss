b57f88d4b61d3c78214ea0c91f6b28ad05d40f35
Dimitrios J. Vergados <djvergad@gmail.com>
2014-10-23 19:42:19 +0300

    Cleaned up configuration file

--- config/initializers/bunny.rb
+++ config/initializers/bunny.rb
@@ -1,11 +1,9 @@
 require 'yaml'
 config = YAML.load_file('config/rabbitmq.yml')
  
-$bunny = Bunny.new(:host => config[Rails.env]["host"],
-        :vhost => config[Rails.env]["vhost"],
-        :user => config[Rails.env]["user"],
-        :password => config[Rails.env]["password"],
-)
+puts "Conncting to RabbitMQ server at host: #{config[Rails.env]["host"]}"
+$bunny = Bunny.new(config[Rails.env].with_indifferent_access)
 
 $bunny.start
 $bunny_channel = $bunny.create_channel
+puts "Connected."
