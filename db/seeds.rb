5db85ba31c1d1034dfdc978bf953f15ea7d5b1aa
Dimitrios J. Vergados <djvergad@gmail.com>
2014-11-12 11:24:05 +0200

    Filled in some initial data in seeds.rb. Run rake db:reset to recreate the dateabase

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = User.create([{email: '***REMOVED***', password: '***REMOVED***'},
+                     {email: '***REMOVED***', password: '***REMOVED***'}])
+users.first.add_role "admin"
+
+
+Interval.create([{ id: 1, duration: 900, name: '15 minutes'},
+                 { id: 2, duration: 3600, name: '1 hour'},
+                 { id: 3, duration: 86400, name: 'Daily'}])
+ 
+clusters = Cluster.create([{name: "Cluster 1", description: "A testing cluster"}])
+
+Prosumer.create([{name: "Prosumer 1", location: "Rio, Patras", cluster: clusters.first }, 
+                 {name: "Prosumer 2", location: "Athens", cluster: clusters.first}]) 
