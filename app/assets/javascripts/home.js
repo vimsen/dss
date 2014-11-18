69afe233b0053d7f82cfc0a600ecb6f2bdc399a8
akretsis <aakretsis@gmail.com>
2014-11-18 18:45:32 +0200

    fix home.js

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -41,8 +41,6 @@ function loadEnergyTypeChart(data){
             defaultTheme: false
         }
     });
-
-	console.log(plotObj);
 }
 
 function energyPriceChart(){
@@ -89,7 +87,6 @@ function totalProsumptionChart(){
             }
         };
 
-/*
         var plotObj = $.plot($("#prosumption-line-chart"), [{
                 data: sin,
                 label: "Production"
@@ -97,66 +94,16 @@ function totalProsumptionChart(){
                 data: cos,
                 label: "Consumption"
             }],
-            options);*/
+            options);
 
     }
-<<<<<<< HEAD
   
 }
-=======
-});
-
-/*
-//Flot Pie Chart
-$(function() {
-
-    var data = [{
-        label: "Series 0",
-        data: 1
-    }, {
-        label: "Series 1",
-        data: 3
-    }, {
-        label: "Series 2",
-        data: 9
-    }, {
-        label: "Series 3",
-        data: 20
-    }];
-
-
-    var plotObj = $.plot($("energy-type-pie-chart"), data, {
-        series: {
-            pie: {
-                show: true
-            }
-        },
-        grid: {
-            hoverable: true
-        },
-        tooltip: true,
-        tooltipOpts: {
-            content: "%p.0%, %s", // show percentages, rounding to 2 decimal places
-            shifts: {
-                x: 20,
-                y: 0
-           },
-            defaultTheme: false
-        }
-    });
->>>>>>> 43893459a0f66af2942621eefc67d9e108f13116
 
 function topConsumers(){
 	
 }
 
-<<<<<<< HEAD
 function topProducers(){
 	
 }
-=======
-
-console.log(":P");
-});
-*/
->>>>>>> 43893459a0f66af2942621eefc67d9e108f13116
