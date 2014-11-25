a4a9dd68db9562fa559c70fc1723a3896158a8da
Dimitrios J. Vergados <djvergad@gmail.com>
2014-11-25 12:35:59 +0200

    Added aggregate in cluster view

--- app/models/cluster.rb
+++ app/models/cluster.rb
@@ -8,12 +8,34 @@ class Cluster < ActiveRecord::Base
   
   def request_cached(interval, startdate, enddate)
     result = []
+    aggregate = {}
+    count = {}
     puts "#{interval}, #{startdate}, #{enddate}"
     self.prosumers.each do |p| 
       p.data_points.where(timestamp: startdate..enddate, interval: interval).order(timestamp: :asc).each do |dp|
-        result.push( dp.clientFormat )  
+        result.push( dp.clientFormat )
+        if aggregate[dp.timestamp].nil?
+          aggregate[dp.timestamp] = DataPoint.new
+          aggregate[dp.timestamp].init_zero dp.timestamp, -1, dp.interval, dp.f_timestamp
+          count[dp.timestamp] = 0  
+        end
+        count[dp.timestamp] += 1
+        aggregate[dp.timestamp].add_data_doint dp
       end
-    end  
+    end
+    
+    num = self.prosumers.count
+    
+    puts "Checking aggregate:"
+    aggregate.each do |key, value|
+      puts key, value
+      if count[key] == num
+        res = value.clientFormat;
+        res[:prosumer_name] = "Aggregate"
+        result.push(res)
+      end
+    end
+    
     
     prosumerlist = self.prosumers.map { |d| d.intelen_id }.join(",")
     
