e954af9811c9fc1c361e4ca4d10b0e1e37a4df6a
Dimitrios J. Vergados <djvergad@gmail.com>
2014-12-02 12:49:58 +0200

    Show different letter on map depending on the cluster

--- app/helpers/prosumers_helper.rb
+++ app/helpers/prosumers_helper.rb
@@ -15,6 +15,13 @@ module ProsumersHelper
     sprintf("%06X", last * (numcolors - cluster.get_icon_index) / (numcolors))
   end
   
+  def letter(cluster)
+    if cluster.nil?
+      return 'N'
+    end
+    cluster.get_icon_index
+  end
+  
   def prosumers_with_locations
     Prosumer.where("location_x IS NOT NULL and location_y IS NOT NULL") 
   end
