d94409e058dac98a3e97e0b13662bdd6978f3ad1
Dimitrios J. Vergados <djvergad@gmail.com>
2014-12-12 12:56:52 +0200

    Changed cluster colors

--- app/helpers/prosumers_helper.rb
+++ app/helpers/prosumers_helper.rb
@@ -6,13 +6,49 @@ module ProsumersHelper
     prosumer
   end
   
+  def color_map size = 10
+    result = []
+        # use golden ratio
+    golden_ratio_conjugate = 0.618033988749895
+    h = golden_ratio_conjugate # use the above as start value
+
+    1.upto size do |i|
+      result[i - 1] = hsv_to_rgb(h, 0.5, 0.95)
+      h += golden_ratio_conjugate
+      h %= 1
+    end
+    result
+  end
+  
   def color(cluster)
     if cluster.nil?
       return '000000'
     end
     numcolors = Cluster.count
-    last = 0xFFFFFF
-    sprintf("%06X", last * (numcolors - cluster.get_icon_index) / (numcolors))
+    sprintf "%06X", tostring((color_map numcolors)[cluster.get_icon_index]) 
+#    last = 0xFFFFFF
+#    sprintf("%06X", last * (numcolors - cluster.get_icon_index) / (numcolors))
+  end
+  
+  def tostring colorArray
+    ((colorArray[0] * 256) + colorArray[1]) * 256 + colorArray[2]
+  end
+  
+  # HSV values in [0..1[
+  # returns [r, g, b] values from 0 to 255
+  def hsv_to_rgb(h, s, v)
+    h_i = (h*6).to_i
+    f = h*6 - h_i
+    p = v * (1 - s)
+    q = v * (1 - f*s)
+    t = v * (1 - (1 - f) * s)
+    r, g, b = v, t, p if h_i==0
+    r, g, b = q, v, p if h_i==1
+    r, g, b = p, v, t if h_i==2
+    r, g, b = p, q, v if h_i==3
+    r, g, b = t, p, v if h_i==4
+    r, g, b = v, p, q if h_i==5
+    [(r*256).to_i, (g*256).to_i, (b*256).to_i]
   end
   
   def letter(cluster)
