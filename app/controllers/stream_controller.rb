9ddd6dfaec7234404cb16476bd18a6670f994abb
Dimitrios J. Vergados <djvergad@gmail.com>
2014-12-18 10:43:46 +0200

    Returned to original. Bug was related to rabbitMQ

--- app/controllers/stream_controller.rb
+++ app/controllers/stream_controller.rb
@@ -90,7 +90,7 @@ class StreamController < ApplicationController
     ActiveRecord::Base.connection.close
     
     loop do
-      sleep 0.2;
+      sleep 1;
       sse.write("OK".to_json, event: 'messages.keepalive')
       ActiveRecord::Base.connection.close
     end
