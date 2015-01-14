b6e4bd1c6395087bd3bdce4eb473cc43290a8e5a
Dimitrios J. Vergados <djvergad@gmail.com>
2015-01-14 17:39:41 +0200

    Added pagination to day ahead index view

--- app/controllers/day_aheads_controller.rb
+++ app/controllers/day_aheads_controller.rb
@@ -5,9 +5,11 @@ class DayAheadsController < ApplicationController
   before_action :set_day_ahead, only: [:show, :edit, :update, :destroy]
   respond_to :json, :html
   load_and_authorize_resource
+  helper_method :sort_column, :sort_direction
 
   def index
-    @day_aheads = DayAhead.all
+    @day_aheads = DayAhead.includes(:prosumer).
+        order(sort_column + ' ' + sort_direction).paginate(page: params[:page])
     respond_with(@day_aheads)
   end
 
@@ -45,11 +47,24 @@ class DayAheadsController < ApplicationController
   end
 
   private
-    def set_day_ahead
-      @day_ahead = DayAhead.find(params[:id])
-    end
 
-    def day_ahead_params
-      params.require(:day_ahead).permit(:prosumer_id, :date)
+  def set_day_ahead
+    @day_ahead = DayAhead.find(params[:id])
+  end
+
+  def day_ahead_params
+    params.require(:day_ahead).permit(:prosumer_id, :date)
+  end
+
+  def sort_column
+    if (DayAhead.column_names + ['prosumers.name']).include?(params[:sort])
+      params[:sort]
+    else
+      'date'
     end
+  end
+
+  def sort_direction
+    %w(asc desc).include?(params[:direction]) ?  params[:direction] : 'desc'
+  end
 end
