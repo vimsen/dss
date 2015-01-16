9bd6bc9a04dd783bb2f2cc4444101735decd6763
Dimitrios J. Vergados <djvergad@gmail.com>
2015-01-16 16:59:50 +0200

    Asset precompile

---
+++ public/assets/chart-d8b758eb3b9bfe7be45ec2c879c9a625.js
@@ -0,0 +1,145 @@
+var plotHelper = (function() {
+  var data = {};
+  var chaanged = false;
+
+  var replot = function(d) {
+    var dataset = [];
+    if (d != null) {
+      $.each(d, function(index, value) {
+        var single = [];
+        $.each(value, function(ind, val) {
+          single.push(val);
+        });
+        dataset.push({
+          label : index,
+          data : single.sort() /*,
+           color : "#00FF00"*/
+        });
+      });
+
+      var s = $("#startDate").length ? Date.parse($('#startDate').val()) : null;
+      var e = $('#realtime').prop('checked')
+                  ? Date.now()
+                  : $("#endDate").length   
+                      ? Date.parse($('#endDate').val()) 
+                      : null;
+
+      // Reduce number of ticks when width is small, to avoid overlapping
+      //var t = $("#placeholder").width() < 450 ? 3 : null;
+
+      if ( $( "#placeholder" ).length ) {
+        $.plot($("#placeholder"), dataset, {
+          series: {
+            lines: {
+              show: true
+            },
+            points: {
+              show: true
+            }
+          },
+          grid: { hoverable: true, clickable: true },
+          tooltip: true,
+          tooltipOpts: {
+              content: "'%s'<br/>%x<br/>%y.2",
+              shifts: {
+                  x: -60,
+                  y: 25
+              }
+          },
+          xaxis : {
+            mode : "time",
+            timeformat : "%d/&#8203;%m/&#8203;%Y<br/>%h:&#8203;%M:&#8203;%S",
+            timezone : "browser",
+            min : s,
+            max : e /*,
+            ticks : t,
+             timeformat : "%y/%m/%d-%h:%M:%S",
+             tickSize : [12, "hour"]*/
+          },
+          yaxis : {
+            tickDecimals: 2
+          },
+          legend:{
+            container: ($( "#legend" ).length ? $("#legend") : null)
+          }
+        });
+      }
+    }
+  };
+
+  var redraw = function(d) {
+    if (changed) {
+      replot(d);
+      changed = false;
+    }
+  };
+
+  var readSingle = function(d, type, forecast, res) {
+    var label = d.prosumer_name + ": " + type;
+    if (res[label] == null) {
+      res[label] = {};
+    }
+    res[label][d.timestamp] = [d.timestamp * 1000, d.actual[type]];
+
+    if (forecast) {
+      var label = d.prosumer_name + ": " + type + ", forecast";
+      if (res[label] == null) {
+        res[label] = {};
+      }
+      res[label][d.timestamp] = [d.forecast.timestamp * 1000, d.forecast[type]];
+    }
+
+    return res;
+  };
+
+  var readData = function(idata, type, forecast) {
+    var result = {};
+
+    if (idata == null) {
+      return result;
+    }
+
+    $.each(idata, function(index, value) {
+      result = readSingle(value, type, forecast, result);
+    });
+    return result;
+  };
+
+  return {
+    replot : replot,
+    readData : readData,
+    drawChart : function(stream, idata, type, forecast) {
+
+      // We set source as global, otherwise we were left
+      // with sources remaining open after visiting internal
+      // pages
+      if (typeof source != "undefined" && source != null) {
+        console.log(source);
+        if (source.OPEN) {
+          source.close();
+          console.log("Closed source");
+        }
+      }
+      source = new EventSource(stream);
+      
+      console.log("Connecting to " + stream);
+   //   console.log(source);
+      data = idata;
+      changed = true;
+
+      $(window).on('resize orientationChanged', function() {
+        replot(data);
+      });
+
+      source.addEventListener('datapoint', function(e) {
+     //   console.log("Datapoint received ", e);
+        var message = JSON.parse(e.data);
+        data = readSingle(message, type, forecast, data);
+        changed = true;
+        window.setTimeout(redraw, 100, data);
+      });
+
+      redraw(data);
+    }
+  };
+})();
