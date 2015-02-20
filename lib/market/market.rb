1fbcde7cef7fc55b5e7f5ea00aabf42957a90f43
Dimitrios J. Vergados <djvergad@gmail.com>
2015-02-20 16:45:39 +0200

    Working on market calculations

--- lib/market/market.rb
+++ lib/market/market.rb
@@ -1,11 +1,49 @@
 module Market
 
-  def optimal_cost(prosumers, startDate, endDate)
-    DataPoint.where(prosumer: prosumers,interval: 2,f_timestamp: startDate .. endDate).group(:f_timestamp).sum(:f_consumption)  end
+  class Calculator
 
-  class Market
+    def initialize(prosumers: nil, startDate: nil, endDate: nil)
+      method(__method__).parameters.each do |type, k|
+        next unless type == :key
+        v = eval(k.to_s)
+        instance_variable_set("@#{k}", v) unless v.nil?
+      end
+    end
+
+    def forecastCost
+      forecast.map do |f|
+        f.fc * forecast_price(f.f_timestamp,f.timestamp)
+
+      end
+    end
+
+    def forecast
+      DataPoint
+          .where(prosumer: @prosumers,
+                 interval: 2,
+                 f_timestamp: @startDate.. @endDate)
+          .group(:timestamp,:f_timestamp)
+          .order(:f_timestamp)
+          .select('timestamp, f_timestamp, sum(consumption) as c, sum(f_consumption) as fc')
+    end
 
 
+
+    def forecast_price(cons_timestamp, fore_timestamp)
+      f_date = cons_timestamp.in_time_zone("UTC").to_date - 1.year
+      f_dayhour = cons_timestamp.in_time_zone("UTC").hour
+
+      DayAheadEnergyPrice.where(date: f_date, dayhour: f_dayhour).count > 0 ?
+          DayAheadEnergyPrice.where(date: f_date, dayhour: f_dayhour).first.price :
+          nil
+    end
+
+    private
+
+    def convert(timestamp)
+
+    end
 
   end
+
 end
