abbefb19e64c07ede15b61c0817df495bc79c14b
Dimitrios J. Vergados <djvergad@gmail.com>
2015-05-12 10:39:24 +0300

    Changed clustering name

--- create_genetic_clustering.rb
+++ create_genetic_clustering.rb
@@ -3,5 +3,5 @@ sd = '24/3/2015 00:00:00 +200'.to_datetime
 ed = '31/3/2015 00:00:00 +200'.to_datetime
 gec = ClusteringModule::GeneticErrorClustering.new(startDate: sd, endDate: ed)
 tcs = gec.run(5)
-cl = Clustering.new(name: "Genetic 24/3/2015-31/3/2015", temp_clusters: tcs)
+cl = Clustering.new(name: "Genetic 24/3/2015-31/3/2015 (2)", temp_clusters: tcs)
 cl.save
