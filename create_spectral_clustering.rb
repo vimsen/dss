c2122892cfe75da893c40521349f78a6a732dd90
Dimitrios J. Vergados <djvergad@gmail.com>
2015-05-12 10:36:28 +0300

    Added create scripts

---
+++ create_spectral_clustering.rb
@@ -0,0 +1,7 @@
+require 'clustering/cross_correlation_error_clustering'
+sd = '24/3/2015 00:00:00 +200'.to_datetime
+ed = '31/3/2015 00:00:00 +200'.to_datetime
+spek = ClusteringModule::CrossCorrelationErrorClustering.new(startDate: sd, endDate: ed)
+cl = Clustering.new(name: "Spectral 24/3/2015-31/3/2015", temp_clusters: spek.run(5))
+cl.save
+exit
