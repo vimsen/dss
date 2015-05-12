6862f6a1fdb228c63367817e482d1bfab6a3276d
Dimitrios J. Vergados <djvergad@gmail.com>
2015-05-12 10:59:44 +0300

    Set fixed date for evaluation

--- eval.rb
+++ eval.rb
@@ -1,5 +1,5 @@
 require 'clustering/evaluate'
-clg = Clustering.find(39)
-eval = ClusteringModule::Evaluator.new(clusters: clg.temp_clusters, startDate: Time.now - 3.months)
+clg = Clustering.find(41)
+eval = ClusteringModule::Evaluator.new(clusters: clg.temp_clusters, startDate: '24/3/2015 00:00:00 +200'.to_datetime)
 eval.evaluate
 
