6b698cdba1899caf3dd881a2af80880b4f39a815
Dimitrios J. Vergados <djvergad@gmail.com>
2015-07-20 18:01:42 +0300

    Match expected output finished

--- lib/clustering/match_expected.rb
+++ lib/clustering/match_expected.rb
@@ -1,9 +1,17 @@
-module CLusteringModule
+
+require "clustering/ai4r_modifications"
+
+module ClusteringModule
   class TargetMatcher
+
+    attr_accessor :targets
+
     def initialize(prosumers: Prosumer.all,
                    startDate: Time.now - 1.day,
                    endDate: Time.now,
-                   interval: 1.hour)
+                   interval: 1.hour,
+                   targets: 25.times.map {|ts| 0}
+      )
 
       method(__method__).parameters.each do |type, k|
         next unless type == :key
@@ -12,16 +20,68 @@ module CLusteringModule
         Rails.logger.debug("key: @#{k}")
         Rails.logger.debug("value: #{v}")
       end
+
+      raise "Wrong targets size" if @targets.length != timestamps.length
+
+      @prosumers = reject_zeros(@prosumers, real_consumption)
+
+    end
+
+    def run
+
+      search = Ai4r::GeneticAlgorithm::GeneticSearchWithOptions.new(
+          200, 100, prosumers: @prosumers,
+          class: Ai4r::GeneticAlgorithm::MatchChromosome,
+          targets: @targets,
+          real_consumption: real_consumption
+      )
+
+      best = search.run
+
+      best.data.zip(@prosumers).each do |ch, pr|
+        puts "Prosumer: #{pr.id}" if ch == 1
+      end
+      []
+    end
+
+    def reject_zeros(prosumers, rc)
+      prosumers.reject do |p|
+        rc[p.id].sum == 0
+      end
+    end
+
+    def timestamps
+      return @timestamps if @timestamps
+      @timestamps = (normalise(@startDate).to_i .. normalise(@endDate).to_i).step(@interval).map do |ts|
+        Time.at(ts)
+      end
+    end
+
+    def normalise(timestamp)
+      case @interval
+        when 1.hour
+          timestamp.beginning_of_hour
+        when 1.day
+          timestamp.beginning_of_day
+        else
+          raise "Wrong interval"
+      end
+
+
     end
 
     def real_consumption
-      result = Hash[DataPoint.where(prosumer: clusters.map{|tc| tc.prosumers}.flatten,
-                                    interval: 2,
-                                    timestamp: ts)
-                        .map do |dp|
-                      [dp.prosumer_id, dp.consumption]
-                    end]
-      puts "result: #{result}"
+      result = Hash[@prosumers.map {|p| [p.id, timestamps.map{|ts| 0}]}]
+      timestamps.map do |ts|
+        DataPoint.where(prosumer: @prosumers,
+                        interval: 2,
+                        timestamp: ts)
+            .map do |dp|
+          result[dp.prosumer_id][timestamps.index(ts)] = dp.consumption
+
+          [dp.prosumer_id, dp.consumption]
+        end
+      end
       result
     end
 
@@ -57,14 +117,19 @@ module Ai4r
 
         total_consumption = @targets.map {|t| 0}
         @data.each_with_index do |d, i|
+       #   puts "d= #{d}, i=#{i}"
+
           if d == 1
-            total_consumption.zip(@real_consumption[@prosumers[i].id]).map do |t,r|
+            total_consumption = total_consumption.zip(@real_consumption[@prosumers[i].id]).map do |t,r|
               t + r
             end
           end
         end
 
-        error += total_consumption.zip(@targets).sum do |r,t|
+        # puts "TOTAL: #{total_consumption}, TARGET: #{@targets}"
+
+        error = total_consumption.zip(@targets).sum do |r, t|
+          # puts "r= #{r}, t= #{t}"
           (r - t).abs
         end
 
@@ -100,7 +165,7 @@ module Ai4r
         #   rand(2) > 0 ? g1 : g2
         # end
 
-        return DynamicChromosome.new(spawn, a.options)
+        return self.new(spawn, a.options)
       end
 
       def self.seed(options)
@@ -113,7 +178,7 @@ module Ai4r
           seed << rand(2)
         end
         #  puts "seed options: #{options[:errors].length}"
-        return DynamicChromosome.new(seed, options)
+        return self.new(seed, options)
       end
 
     end
