9309ee41ca75fc1b9e3ab61fb81cd98cfa2549d2
Dimitrios J. Vergados <djvergad@gmail.com>
2015-09-25 17:14:29 +0300

    Sign in before retrieving data
    Former-commit-id: 0624b4f8029b33597c9871b6d22866ee22a5a969

--- test/controllers/market_prices_controller_test.rb
+++ test/controllers/market_prices_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class MarketPricesControllerTest < ActionController::TestCase
+
+  setup do
+    sign_in User.first
+  end
+
   test "should get index" do
     get :index
     assert_response :success
