9a71156beba134a954572834f2b774407d33e477
Dimitrios J. Vergados <djvergad@gmail.com>
2015-09-25 15:37:29 +0300

    Made first user an admin in tests
    Former-commit-id: 684a22f09a9f30a43008f0d07e972c0bf36f68cc

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,9 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  setup do
+    User.first.add_role :admin
+  end
 end
 
 class ActionController::TestCase
