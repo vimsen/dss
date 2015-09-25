177ac888002024dd720e478c687b42757cff4f35
Dimitrios J. Vergados <djvergad@gmail.com>
2015-09-25 16:55:59 +0300

    Template for clusteringcontrollertest. Needs more tests

--- test/controllers/clustering_controller_test.rb
+++ test/controllers/clustering_controller_test.rb
@@ -1,18 +1,25 @@
 require 'test_helper'
 
 class ClusteringControllerTest < ActionController::TestCase
+
+  def setup
+    @controller = ClusteringsController.new
+    sign_in User.first
+  end
+
+  test "should get index" do
+
+    get :index
+    assert_response :success
+  end
+
   test "should get select" do
     get :select
     assert_response :success
   end
 
   test "should get confirm" do
-    get :confirm
-    assert_response :success
-  end
-
-  test "should get save" do
-    get :save
+    post :confirm, algorithm: :energy_type
     assert_response :success
   end
 
