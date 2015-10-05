c170d7fb7860288ae25bb4989d34ae4fd7bba757
Dimitrios J. Vergados <djvergad@gmail.com>
2015-10-05 14:27:52 +0300

    Data for specific date interval
    Former-commit-id: ee898b9470a6c547cb2703d5a76794b9a96ac4ad

--- test/unit/clustering/spectral_clustering_test.rb
+++ test/unit/clustering/spectral_clustering_test.rb
@@ -1,42 +1,27 @@
 require 'test_helper'
+require 'test_helper_with_prosumption_data'
 require 'clustering/spectral_clustering'
 
-class SpectralClusteringTest < ActionController::TestCase
+class SpectralClusteringTest < ActiveSupport::TestCaseWithProsumptionData
 
   test "should create spectral clustering" do
 
-    ed = DateTime.now
-    sd = ed - 1.week
+    ed = '2015/3/30'
+    sd = '2015/3/23'
 
-    prosumers = 1.upto(37).map do |i|
-       Prosumer.create(intelen_id: i)
-    end
+    spek = ClusteringModule::SpectralClustering.new(prosumers: @prosumers, startDate: sd, endDate: ed)
 
-    puts "#{prosumers.map{|p| [p.id, p.intelen_id]}}"
-
-    prosumers.each_slice(5) do |p|
-      puts "#{p.map{|p| [p.id, p.intelen_id]}}"
-      begin
-        FetchAsynch::DownloadAndPublish.new(p , 2, sd, ed, nil, true)
-      rescue
-        puts "Failed to downlad data"
-
-      end
-    end
-
-    spek = ClusteringModule::SpectralClustering.new(prosumers: prosumers, startDate: sd, endDate: ed)
-
-    1.upto(9) do |i|
-
-      puts Clustering.count
+    2.upto(9) do |i|
       assert_difference('Clustering.count') do
         cl = Clustering.new(name: "Spectral k=#{i}", temp_clusters: spek.run(i))
         cl.save
         assert_equal(cl.temp_clusters.count, i)
-        puts Clustering.count
+        puts "#{cl.temp_clusters.map{|tc| tc.prosumers.map{|p| @prosumers.index(Prosumer.find(p.id))}}}"
+        stats = spek.stats(cl.temp_clusters.map{|tc| tc.prosumers.map{|p| @prosumers.index(Prosumer.find(p.id))}})
+        puts "#{stats}"
+        assert_operator(stats[:ingroup], :>, stats[:outgroup])
         Clustering.count
       end
-      puts Clustering.count
     end
 
   end
