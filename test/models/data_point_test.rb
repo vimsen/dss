471ac89edbd6ec845fd9a9f229319ccb339146d4
Dimitrios J. Vergados <djvergad@gmail.com>
2015-10-05 14:27:13 +0300

    Test that prosumption data is inserted
    Former-commit-id: 0a4520f7c2d0217cfcb4d5ac8e2335613492afd3

--- test/models/data_point_test.rb
+++ test/models/data_point_test.rb
@@ -1,8 +1,17 @@
 require 'test_helper'
+require 'test_helper_with_prosumption_data'
 
-class DataPointTest < ActiveSupport::TestCase
+class DataPointTest < ActiveSupport::TestCaseWithProsumptionData
 
-  test "DataPoints should have "
+  test "DataPoints should have 16266 data_points" do
+    assert_equal 30944, DataPoint.count
+
+    @prosumers.each do |p|
+      puts "#{p.id}: #{p.name}: #{p.intelen_id}: #{p.data_points.where(interval: 2).count}"
+    end
+  end
+
+
 
 #  test "the truth" do
 #     assert true
