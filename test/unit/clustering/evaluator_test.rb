89b080ea7aa4d4401bddb5f1fab7b4106d65b53a
Dimitrios J. Vergados <djvergad@gmail.com>
2015-10-06 16:11:04 +0300

    Added evaluator test

---
+++ test/unit/clustering/evaluator_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+require 'test_helper_with_pros_and_market_data'
+require 'clustering/evaluate'
+require 'clustering/spectral_clustering'
+
+class EvaluatorTest < ActiveSupport::TestCaseWithProsAndMarketData
+  test "should create evaluation results" do
+
+    training_ed = '2015/3/30'
+    training_sd = '2015/3/23'
+
+    spek = ClusteringModule::SpectralClustering.new(prosumers: @prosumers,
+                                                    startDate: training_sd,
+                                                    endDate: training_ed)
+
+    clg = Clustering.new(name: "Spectral", temp_clusters: spek.run(5))
+    clg.save
+
+    puts "#{clg.temp_clusters.map{|tc| tc.prosumers.map{|p| @prosumers.index(Prosumer.find(p.id))}}}"
+
+    eval = ClusteringModule::Evaluator.new(
+        clusters: clg.temp_clusters,
+        startDate: '30/3/2015 00:00:00 +200'.to_datetime,
+        endDate: '27/4/2015 00:00:00 +200'.to_datetime,
+        adaptive: false,
+        interval: 1.week,
+        outputFile: 'results/test_',
+        runs: 1
+    )
+
+    eval.evaluate
+
+    assert false
+
+  end
+end
