1f7e91ebc7d9e4ab4a99891396b4c5243217d7ce
Dimitrios J. Vergados <djvergad@gmail.com>
2015-10-06 15:44:06 +0300

    Added more checks to datapoint model test
    Former-commit-id: 6aae0c90d3e4643dbbd1aed73bafe87e44b87fbf

--- test/models/data_point_test.rb
+++ test/models/data_point_test.rb
@@ -4,11 +4,30 @@ require 'test_helper_with_prosumption_data'
 class DataPointTest < ActiveSupport::TestCaseWithProsumptionData
 
   test "DataPoints should have 16266 data_points" do
-    assert_equal 30944, DataPoint.count
+    assert_equal 241809, DataPoint.count
 
-    @prosumers.each do |p|
-      puts "#{p.id}: #{p.name}: #{p.intelen_id}: #{p.data_points.where(interval: 2).count}"
+    startdate = '2015/3/23'.to_datetime
+    enddate = '2015/4/27'.to_datetime
+
+=begin
+    @prosumers.sort_by{ |p| p.name }.each do |p|
+      puts "#{p.id}: #{p.name}: #{p.intelen_id}: #{p.data_points.where(
+               interval: 2,
+               timestamp: startdate .. enddate
+           ).count}"
     end
+=end
+
+    points = @prosumers.map do |p|
+      p.data_points.where(
+          interval: 2,
+          timestamp: startdate .. enddate).count
+    end.sum
+
+    max = @prosumers.count * (startdate .. enddate).count * 24
+
+    assert_operator(points, :>, max / 2)
+
   end
 
 
