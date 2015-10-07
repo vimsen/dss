671a30ff60150360a68736bfa50bacff11e19f6d
Dimitrios J. Vergados <djvergad@gmail.com>
2015-10-07 17:51:26 +0300

    Moved spectral clustering algorithm to a seperate class

--- lib/clustering/spectral_clustering.rb
+++ lib/clustering/spectral_clustering.rb
@@ -39,10 +39,72 @@ module ClusteringModule
                        [ [pid, timestamp], v - (@forecasts[[pid, timestamp]] || 0)]
                      end]
 
-      @similarity_matrix = Matrix.build(@prosumers.length, @prosumers.length)  do |row, col|
+      @similarity_matrix = generate_similarity_matrix
+
+    end
+
+    def run(kappa = 5)
+
+      sc = SpectralClustering.new(@similarity_matrix)
+
+      clusters = sc.run(kappa)
+
+      clusters.map.with_index do |cl, i|
+        TempCluster.new(name: "Spectral #{i}",
+                        description: "Spectral error clustering #{i}",
+                        prosumers: cl.map { |p| @prosumers[p]})
+      end
+    end
+
+    def generate_similarity_matrix
+      Matrix.build(@prosumers.length, @prosumers.length)  do |row, col|
         cross_correlation(@prosumers[row].id, @prosumers[col].id) # We need similarities to always be positive
       end
+    end
 
+    def cross_correlation(pid1, pid2)
+      common_timestamps = @timestamps[pid1] & @timestamps[pid2]
+
+      s12 = common_timestamps.sum{|ts| @errors[[pid1, ts]] * @errors[[pid2, ts]]}
+      s11 = common_timestamps.sum{|ts| @errors[[pid1, ts]] ** 2 }
+      s22 = common_timestamps.sum{|ts| @errors[[pid2, ts]] ** 2 }
+
+      return 0 if (s11 == 0 || s22 == 0)
+      s12 / ( s11 ** 0.5 * s22 ** 0.5)
+    end
+
+    def stats(clusters)
+      sum_same = sum_different = 0.0;
+      count_same = count_different = 0
+      @prosumers.combination(2) do |pi,pj|
+        i = @prosumers.index(pi)
+        j = @prosumers.index(pj)
+
+        if same_cluster(clusters, i,j)
+          sum_same += @similarity_matrix[i,j]
+          count_same += 1
+        else
+          sum_different += @similarity_matrix[i,j]
+          count_different += 1
+        end
+      end
+      {
+          ingroup: sum_same / count_same,
+          outgroup: sum_different / count_different
+      }
+    end
+
+    def same_cluster(clusters, i,j)
+      clusters.find{|k| k.include? i} == clusters.find{|k| k.include? j}
+    end
+
+
+  end
+
+
+  class SpectralClustering
+    def initialize(similarity_matrix)
+      @similarity_matrix = similarity_matrix
     end
 
     def run(kappa = 5)
@@ -85,64 +147,18 @@ module ClusteringModule
         break if centroids <=> old_centroids
       end
 
-      clusters.map.with_index do |cl, i|
-        TempCluster.new(name: "Spectral #{i}",
-                        description: "Spectral error clustering #{i}",
-                        prosumers: cl.map { |p| @prosumers[p]})
-      end
+      clusters
     end
 
-    def cross_correlation(pid1, pid2)
-      common_timestamps = @timestamps[pid1] & @timestamps[pid2]
-
-      s12 = common_timestamps.sum{|ts| @errors[[pid1, ts]] * @errors[[pid2, ts]]}
-      s11 = common_timestamps.sum{|ts| @errors[[pid1, ts]] ** 2 }
-      s22 = common_timestamps.sum{|ts| @errors[[pid2, ts]] ** 2 }
-
-      return 0 if (s11 == 0 || s22 == 0)
-      s12 / ( s11 ** 0.5 * s22 ** 0.5)
-    end
+    private
 
     def get_centroid(cluster, y)
       cluster.sum { |i| y[i] } / cluster.length
     end
 
-
     def find_closest(vector, centroids)
       centroids.index(centroids.min_by {|c| (c - vector).magnitude })
     end
-
-    def stats(clusters)
-      sum_same = sum_different = 0.0;
-      count_same = count_different = 0
-      @prosumers.combination(2) do |pi,pj|
-        i = @prosumers.index(pi)
-        j = @prosumers.index(pj)
-
-        if same_cluster(clusters, i,j)
-          sum_same += @similarity_matrix[i,j]
-          count_same += 1
-        else
-          sum_different += @similarity_matrix[i,j]
-          count_different += 1
-        end
-      end
-      {
-          ingroup: sum_same / count_same,
-          outgroup: sum_different / count_different
-      }
-    end
-
-    def same_cluster(clusters, i,j)
-      clusters.find{|k| k.include? i} == clusters.find{|k| k.include? j}
-    end
-
-
-  end
-
-
-  class SpectralClustering
-
 
   end
 end
