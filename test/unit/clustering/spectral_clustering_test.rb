447b64ed6592281f410c4168f531c48112d9f601
Dimitrios J. Vergados <djvergad@gmail.com>
2015-10-08 17:53:18 +0300

    Created different clustering matrices

--- test/unit/clustering/spectral_clustering_test.rb
+++ test/unit/clustering/spectral_clustering_test.rb
@@ -4,23 +4,39 @@ require 'clustering/spectral_clustering'
 
 class SpectralClusteringTest < ActiveSupport::TestCaseWithProsumptionData
 
-  test "should create spectral clustering" do
-
+  test "should create Cross-correlation error spectral clustering" do
     spek = ClusteringModule::CrossCorrelationErrorClustering.new(prosumers: @prosumers, startDate: @startdate, endDate: @trainend)
+    check_performance spek
+  end
 
+  test "should create inverse Cross-correlation error spectral clustering" do
+    spek = ClusteringModule::InverseCrossCorrelationErrorClustering.new(prosumers: @prosumers, startDate: @startdate, endDate: @trainend)
+    check_performance spek
+  end
+
+  test "should create Cross-correlation consumption spectral clustering" do
+    spek = ClusteringModule::CrossCorrelationConsumptionClustering.new(prosumers: @prosumers, startDate: @startdate, endDate: @trainend)
+    check_performance spek
+  end
+
+  test "should create inverse Cross-correlation consumption spectral clustering" do
+    spek = ClusteringModule::InverseCrossCorrelationConsumptionClustering.new(prosumers: @prosumers, startDate: @startdate, endDate: @trainend)
+    check_performance spek
+  end
+
+  def check_performance clusteringModule
     2.upto(9) do |i|
       assert_difference('Clustering.count') do
-        cl = Clustering.new(name: "Spectral k=#{i}", temp_clusters: spek.run(i))
+        cl = Clustering.new(name: "Spectral k=#{i}", temp_clusters: clusteringModule.run(i))
         cl.save
         assert_equal(cl.temp_clusters.count, i)
         puts "#{cl.temp_clusters.map{|tc| tc.prosumers.map{|p| @prosumers.index(Prosumer.find(p.id))}}}"
-        stats = spek.stats(cl.temp_clusters.map{|tc| tc.prosumers.map{|p| @prosumers.index(Prosumer.find(p.id))}})
+        stats = clusteringModule.stats(cl.temp_clusters.map{|tc| tc.prosumers.map{|p| @prosumers.index(Prosumer.find(p.id))}})
         puts "#{stats}"
         assert_operator(stats[:ingroup], :>, stats[:outgroup])
         Clustering.count
       end
     end
-
   end
 
 end
