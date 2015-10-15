52a7a6f2fadea2e9221a8751b6be68c67e9df2dc
Dimitrios J. Vergados <djvergad@gmail.com>
2015-10-15 12:52:46 +0300

    Renamed some test cases to match tested class

--- test/unit/clustering/spectral_clustering_test.rb
+++ test/unit/clustering/spectral_clustering_test.rb
@@ -4,27 +4,27 @@ require 'clustering/spectral_clustering'
 
 class SpectralClusteringTest < ActiveSupport::TestCaseWithProsumptionData
 
-  test "should create Cross-correlation error spectral clustering" do
+  test "should create positive error spectral clustering" do
     spek = ClusteringModule::PositiveErrorSpectralClustering.new(prosumers: @prosumers, startDate: @startdate, endDate: @trainend)
     check_performance spek
   end
 
-  test "should create inverse Cross-correlation error spectral clustering" do
+  test "should create negative error spectral clustering" do
     spek = ClusteringModule::NegativeErrorSpectralClustering.new(prosumers: @prosumers, startDate: @startdate, endDate: @trainend)
     check_performance spek
   end
 
-  test "should create Cross-correlation consumption spectral clustering" do
+  test "should create positive consumption spectral clustering" do
     spek = ClusteringModule::PositiveConsumptionSpectralClustering.new(prosumers: @prosumers, startDate: @startdate, endDate: @trainend)
     check_performance spek
   end
 
-  test "should create inverse Cross-correlation consumption spectral clustering" do
+  test "should create negative consumption spectral clustering" do
     spek = ClusteringModule::NegativeConsumptionSpectralClustering.new(prosumers: @prosumers, startDate: @startdate, endDate: @trainend)
     check_performance spek
   end
 
-  def check_performance clusteringModule
+  def check_performance(clusteringModule)
     2.upto(9) do |i|
       assert_difference('Clustering.count') do
         cl = Clustering.new(name: "Spectral k=#{i}", temp_clusters: clusteringModule.run(i))
