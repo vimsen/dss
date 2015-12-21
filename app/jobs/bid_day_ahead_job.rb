c303e8f79c3acf644f5a92b269fd7b25d11a23d8
Dimitrios J. Vergados <djvergad@gmail.com>
2015-12-21 16:35:36 +0200

    MAking job to communicate

--- app/jobs/bid_day_ahead_job.rb
+++ app/jobs/bid_day_ahead_job.rb
@@ -1,9 +1,24 @@
+require 'rest_client'
+require 'yaml'
+
 class BidDayAheadJob < ActiveJob::Base
   queue_as :default
 
   def perform(*args)
 
-    puts "The number of data points is #{DataPoint.count}"
+    config = YAML.load_file('config/config.yml')
+
+    user = config[Rails.env]["market_operator"]["user"]
+    token = config[Rails.env]["market_operator"]["token"]
+    base_uri = config[Rails.env]["market_operator"]["host"]
+
+    uri = "#{base_uri}/bids/"
+
+    rest_resource = RestClient::Resource.new(uri, verify_ssl: OpenSSL::SSL::VERIFY_NONE)
+
+    puts JSON.pretty_generate rest_resource.get
+
+    puts "The number of data points is #{DataPoint.count}, user is #{user}."
     # Do something later
   end
 end
