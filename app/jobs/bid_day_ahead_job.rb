df6731ceb197f2cfb2291452a7b3dd0183925aeb
Dimitrios J. Vergados <djvergad@gmail.com>
2016-01-05 16:48:20 +0200

    Working on bidding

--- app/jobs/bid_day_ahead_job.rb
+++ app/jobs/bid_day_ahead_job.rb
@@ -1,4 +1,4 @@
-require 'rest_client'
+require 'rest-client'
 require 'yaml'
 require 'fetch_asynch/download_and_publish'
 
@@ -7,8 +7,10 @@ class BidDayAheadJob < ActiveJob::Base
 
   def perform(*args)
 
-    FetchAsynch::DownloadAndPublish.new(Prosumer.all, 2, Date.yesterday, Time.now, nil, true)
+    puts "Downloading data"
+    FetchAsynch::DownloadAndPublish.new(Prosumer.all, 2, Date.yesterday.to_datetime, Time.now, nil, true)
 
+    puts "Downloaded data"
     config = YAML.load_file('config/config.yml')
 
     user = config[Rails.env]["market_operator"]["user"]
@@ -19,29 +21,38 @@ class BidDayAheadJob < ActiveJob::Base
 
     markets = JSON.parse rest_resource['markets'].get params: {user_email: user, user_token: token, format: :json}
 
+    puts "Downloaded markets"
+
     day_ahead_market = (markets.find do |m|
       m["name"] == "Day Ahead"
     end)
 
+    puts "found day ahead market"
     newbid = {
         market_id: day_ahead_market["id"],
         date: Date.tomorrow.to_s,
         bid_items_attributes: day_ahead_market["blocks"].map do |b|
+          vol = DataPoint.where(f_timestamp: Date.tomorrow.to_datetime + b["starting"].seconds).map{|dp| (dp.f_consumption - dp.f_production)}.sum
           {
               block_id: b["id"].to_i,
-              volume: rand(10.0...200.0),
+              volume: vol,
               price: rand(1.0...20.0)
           }
         end
     }
+    puts "created bid"
     request_object = {
         bid: newbid, user_email: user, user_token: token
     }
-    # puts day_ahead_market["blocks"], request_object.to_json
+    puts "created request object"
+    puts day_ahead_market["blocks"], request_object.to_json
 
-    bid = JSON.parse rest_resource['bids'].post request_object.to_json, :content_type => :json, :accept => :json
+    result = rest_resource['bids'].post(request_object.to_json, :content_type => :json, :accept => :json)
+    puts "The result is #{result}"
+   # bid = JSON.parse(rest_resource['bids'].post(request_object.to_json, :content_type => :json, :accept => :json))
+   # puts "Posted new bid"
 
-    puts "The number of data points is #{DataPoint.count}, user is #{user}. Bid is #{bid}."
+   # puts "The number of data points is #{DataPoint.count}, user is #{user}. Bid is #{bid}."
     # Do something later
   end
 end
