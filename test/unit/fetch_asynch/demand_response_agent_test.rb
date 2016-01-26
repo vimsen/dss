5a2268106d63d6fe1ae0bba39840360a93d374ca
Dimitrios J. Vergados <djvergad@gmail.com>
2016-01-26 12:05:50 +0200

    GDRMS interface
    Former-commit-id: ba13192c35ab117f4776a801138e7c3d3d8a53c2

--- test/unit/fetch_asynch/demand_response_agent_test.rb
+++ test/unit/fetch_asynch/demand_response_agent_test.rb
@@ -1,16 +1,31 @@
 require 'test_helper'
+require 'test_helper_with_prosumption_data'
 
-class DemandResponseAgentTest < ActiveSupport::TestCase
+class DemandResponseAgentTest < ActiveSupport::TestCaseWithProsumptionData
 
   setup do
     # Do nothing
+    @dr_obj = demand_responses(:one)
+    @agent = FetchAsynch::DemandResponseAgent.new
   end
 
   test "Should post dr_event" do
 
-    dr_obj = demand_responses(:one)
-    agent = FetchAsynch::DemandResponseAgent.new
-    agent.dr_activation dr_obj.id
-    puts dr_obj.as_json(include: :dr_targets)
+    @agent = FetchAsynch::DemandResponseAgent.new
+    @agent.dr_activation @dr_obj.id
+
+    @dr_obj.reload
+    assert_not_nil @dr_obj.plan_id, "Plan id should not be nil"
+    assert_operator 0, :<, @dr_obj.plan_id, "Plan id should be a positive number"
+
+    @agent.refresh_status @dr_obj.id
+
+    @dr_obj.reload
+    # assert_operator 0, :<, @dr_obj.dr_planneds.count, "We should get some plans"
+    # assert_operator 0, :<, @dr_obj.dr_actuals.count, "We should get some actuals"
+
+
+
   end
+
 end
