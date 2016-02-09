2dcc3943c73f94301d70c2e3edb2671fb2699df4
Dimitrios J. Vergados <djvergad@gmail.com>
2016-02-09 14:24:00 +0200

    Slice prosumption data
    Former-commit-id: ef328156163c93d91090a7f0196add7d4482fa77

--- lib/fetch_asynch/download_and_publish.rb
+++ lib/fetch_asynch/download_and_publish.rb
@@ -36,11 +36,7 @@ module FetchAsynch
             x = nil
           end
 
-
           ActiveRecord::Base.connection_pool.with_connection do
-
-
-
 
             params = { # prosumers: prosumers.map {|p| p.intelen_id}.reject{|id| is_integer? id },
                        startdate: startdate.to_s,
@@ -53,13 +49,16 @@ module FetchAsynch
               puts i; i=i+1;
               puts "Hello"
               new_api_prosumer_ids. each do |id|
+                RestClient.log = $stdout
                 rest_resource = RestClient::Resource.new(u)
                 raw = rest_resource['getdataVGW'].get params: params.merge(prosumers: id)
-               #  Rails.logger.debug "RAW: #{raw}"
+                Rails.logger.debug "RAW: #{raw}"
                 result = JSON.parse raw
                #  Rails.logger.debug "Result: #{result}"
                 result_conv = convert_new_to_old_api result
                #  Rails.logger.debug "Result_conv: #{result_conv}"
+                x.publish({data:  "Interval #{@interval.name}: Processing results for prosumers: #{id}.", event: "output"}.to_json) if x
+                Rails.logger.debug "Interval #{@interval.name}: Processing results for prosumers: #{id}."
                 datareceived(result_conv, x)
               end
               # datareceived_new(result, channel)
@@ -70,15 +69,20 @@ module FetchAsynch
               puts "OLD API"
               uri = URI.parse(u + '/getdata')
               puts i; i=i+1;
-              uri.query = URI.encode_www_form(params.merge prosumers: old_api_prosumer_ids.join(","))
-              puts i; i=i+1;
 
-              puts "In the new Thread..."
-              Rails.logger.debug "Connecting to: #{uri}"
-              raw = uri.open.read
-            #   Rails.logger.debug "RAW: #{raw}"
-              result = JSON.parse(raw)
-              datareceived(result, x)
+              old_api_prosumer_ids.each_slice(10) do |slice|
+                uri.query = URI.encode_www_form(params.merge prosumers: slice.join(","))
+                puts i; i=i+1;
+
+                puts "In the new Thread..."
+                Rails.logger.debug "Connecting to: #{uri}"
+                raw = uri.open.read
+                #   Rails.logger.debug "RAW: #{raw}"
+                result = JSON.parse(raw)
+                x.publish({data:  "Interval #{@interval.name}: Processing results for prosumers: #{slice.join(",")}.", event: "output"}.to_json) if x
+                Rails.logger.debug "Interval #{@interval.name}: Processing results for prosumers: #{slice.join(",")}."
+                datareceived(result, x)
+              end
             end
 
           end
@@ -101,7 +105,8 @@ module FetchAsynch
           Rails.logger.debug "market data published."
           Rails.logger.debug 'done'
         rescue => e
-          Rails.logger.debug e
+          Rails.logger.debug "EXCEPTION: #{e.inspect}"
+          Rails.logger.debug "MESSAGE: #{e.message}"
           Rails.logger.debug e.backtrace.join("\n")
         end
       end
@@ -143,8 +148,8 @@ module FetchAsynch
             },
             "dr" => nil,
             "reliability" => nil
-        }
-      end
+        } if is_integer? d["Kwh"].to_s
+      end.compact
    end
 
     def datareceived(data, x)
@@ -158,27 +163,40 @@ module FetchAsynch
       ActiveRecord::Base.connection_pool.with_connection do | conn |
         procs = Hash[@prosumers.map {|p| [p.intelen_id, p]}]
 
-        Upsert.batch(conn, DataPoint.table_name) do |upsert|
-          data.each do | d |
-            # puts "Data Point: #{d}"
-            upsert.row({
-                           timestamp: d['timestamp'].to_datetime,
-                           prosumer_id: procs[d['procumer_id'].to_s].id,
-                           interval_id: @interval.id
-                       }, {
-                           production: d['actual']['production'],
-                           consumption: d['actual']['consumption'],
-                           storage: d['actual']['storage'],
-                           f_timestamp: d['forecast']['timestamp'].to_datetime,
-                           f_production: d['forecast']['production'],
-                           f_consumption: d['forecast']['consumption'],
-                           f_storage: d['forecast']['storage'],
-                           dr: d['dr'],
-                           reliability: d['reliability']
-                       }
-            ) if valid_time_stamp d['timestamp']
+        begin
+          upsert_status = Upsert.batch(conn, DataPoint.table_name) do |upsert|
+            data.each do | d |
+              # puts "Data Point: #{d}"
+              upsert.row({
+                             timestamp: d['timestamp'].to_datetime,
+                             prosumer_id: procs[d['procumer_id'].to_s].id,
+                             interval_id: @interval.id
+                         }, {
+                             production: d['actual']['production'],
+                             consumption: d['actual']['consumption'],
+                             storage: d['actual']['storage'],
+                             f_timestamp: d['forecast']['timestamp'].to_datetime,
+                             f_production: d['forecast']['production'],
+                             f_consumption: d['forecast']['consumption'],
+                             f_storage: d['forecast']['storage'],
+                             dr: d['dr'],
+                             reliability: d['reliability']
+                         }
+              ) if valid_time_stamp d['timestamp']
+            end
           end
+          x.publish({data:  "Interval #{@interval.name}: UPSERT status: #{upsert_status.count}", event: "output"}.to_json) if x
+          Rails.logger.debug "Interval #{@interval.name}: UPSERT status: #{upsert_status.count}"
+        rescue PG::InvalidTextRepresentation => e
+          x.publish({data:  "Interval #{@interval.name}: BAD DATA FORMAT: #{upsert_status}", event: "output"}.to_json)
+          # x.publish({data:  "BAD DATA FORMAT: #{data}", event: "output"}.to_json)
+          Rails.logger.debug "Interval #{@interval.name}: BAD DATA FORMAT: #{upsert_status}"
+          # Rails.logger.debug "BAD DATA FORMAT: #{data}"
+          raise e
         end
+
+
+
 
         new_data_points = data.reject do |d|
           !valid_time_stamp(d['timestamp']) || d['timestamp'].to_datetime.future?
