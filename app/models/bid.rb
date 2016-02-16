c6564563192ee94c3596c92e348ae2f674607499
Dimitrios J. Vergados <djvergad@gmail.com>
2016-02-16 15:29:59 +0200

    Added job that checks bid status

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,3 +1,4 @@
 class Bid < ActiveRecord::Base
-  has_many :sla_items
+  has_many :sla_items, dependent: :destroy
+  enum status: %i(submitted accepted rejected withdrawn)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160215171453) do
+ActiveRecord::Schema.define(version: 20160216100223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,10 @@ ActiveRecord::Schema.define(version: 20160215171453) do
     t.integer  "mo_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "status"
   end
+
+  add_index "bids", ["status"], name: "index_bids_on_status", using: :btree
 
   create_table "building_types", force: :cascade do |t|
     t.string   "name"
