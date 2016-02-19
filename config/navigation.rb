31207f1fc4bfeb16cbbeaa3f9f5b7911e5de3c6c
Dimitrios J. Vergados <djvergad@gmail.com>
2016-02-19 14:57:08 +0200

    Added SLA monitoring page
    Former-commit-id: 53c581e639d8def68a25007d711433c29a05761d

--- config/navigation.rb
+++ config/navigation.rb
@@ -94,6 +94,7 @@ SimpleNavigation::Configuration.run do |navigation|
 
     primary.item :menu_demand_responses, 'Demand Responses', demand_responses_path
     primary.item :menu_bids, 'Bids', bids_path
+    primary.item :menu_sla, 'Monitor SLA progress', sla_monitor_path
 
     primary.item :menu_meters, 'Meters', '#' do |sub_nav|
       HierMenu::HierMenu.new('meters_hier', Proc.new { |p| meter_url(p) }, :mac, 4).fill_node sub_nav, Meter.all.order(id: :asc)
