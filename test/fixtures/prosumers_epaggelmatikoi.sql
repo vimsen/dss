04185301e505cdbbc980d0b069ba8b811c8c68fb
Dimitrios J. Vergados <djvergad@gmail.com>
2016-09-23 15:11:23 +0300

    Change prosumer name to include HEDNO prefix

--- test/fixtures/prosumers_epaggelmatikoi.sql
+++ test/fixtures/prosumers_epaggelmatikoi.sql
@@ -1,50 +1,50 @@
-8001	108001	epaggelmatikoi_8001	Δυτ. Θεσ/κης
-8002	108002	epaggelmatikoi_8002	Κορωπίου
-8003	108003	epaggelmatikoi_8003	Καβάλας
-8004	108004	epaggelmatikoi_8004	Ν.Ιωνιας
-8005	108005	epaggelmatikoi_8005	Βόλου
-8006	108006	epaggelmatikoi_8006	Δυτ. Θεσ/κης
-8007	108007	epaggelmatikoi_8007	Κορωπίου
-8008	108008	epaggelmatikoi_8008	Κορωπίου
-8009	108009	epaggelmatikoi_8009	Κορωπίου
-8010	108010	epaggelmatikoi_8010	Πάτρας
-8011	108011	epaggelmatikoi_8011	Πάτρας
-8012	108012	epaggelmatikoi_8012	Δυτ. Θεσ/κης
-8013	108013	epaggelmatikoi_8013	Ανατ. Θεσ/κης
-8014	108014	epaggelmatikoi_8014	Φλώρινας
-8015	108015	epaggelmatikoi_8015	Καλλιθέας
-8016	108016	epaggelmatikoi_8016	Αιγάλεω
-8017	108017	epaggelmatikoi_8017	Ανατ. Θεσ/κης
-8018	108018	epaggelmatikoi_8018	Κορωπίου
-8019	108019	epaggelmatikoi_8019	Ανατ. Θεσ/κης
-8020	108020	epaggelmatikoi_8020	Πάτρας
-8021	108021	epaggelmatikoi_8021	Πάτρας
-8022	108022	epaggelmatikoi_8022	Πάτρας
-8023	108023	epaggelmatikoi_8023	Κ.Αχαϊας
-8024	108024	epaggelmatikoi_8024	Βόλου
-8025	108025	epaggelmatikoi_8025	Αγρινίου
-8026	108026	epaggelmatikoi_8026	Καβάλας
-8027	108027	epaggelmatikoi_8027	Καβάλας
-8028	108028	epaggelmatikoi_8028	Γλυφάδας
-8029	108029	epaggelmatikoi_8029	Δυτ. Θεσ/κης
-8030	108030	epaggelmatikoi_8030	Ανατ. Θεσ/κης
-8031	108031	epaggelmatikoi_8031	Πάτρας
-8032	108032	epaggelmatikoi_8032	Δυτ. Θεσ/κης
-8033	108033	epaggelmatikoi_8033	Κορωπίου
-8034	108034	epaggelmatikoi_8034	Καλλιθέας
-8035	108035	epaggelmatikoi_8035	Αιγάλεω
-8036	108036	epaggelmatikoi_8036	Πάτρας
-8037	108037	epaggelmatikoi_8037	Λάρισας
-8038	108038	epaggelmatikoi_8038	Δυτ. Θεσ/κης
-8039	108039	epaggelmatikoi_8039	Δυτ. Θεσ/κης
-8040	108040	epaggelmatikoi_8040	Αιγίου
-8041	108041	epaggelmatikoi_8041	Βόλου
-8042	108042	epaggelmatikoi_8042	Φλώρινας
-8043	108043	epaggelmatikoi_8043	Φλώρινας
-8044	108044	epaggelmatikoi_8044	Λάρισας
-8045	108045	epaggelmatikoi_8045	Κορωπίου
-8046	108046	epaggelmatikoi_8046	Κορωπίου
-8047	108047	epaggelmatikoi_8047	Βόλου
-8048	108048	epaggelmatikoi_8048	Αιγίου
-8049	108049	epaggelmatikoi_8049	Ανατ. Θεσ/κης
-8050	108050	epaggelmatikoi_8050	Αιγίου
+8001	8001	HEDNO_epaggelmatikoi_8001	Δυτ. Θεσ/κης
+8002	8002	HEDNO_epaggelmatikoi_8002	Κορωπίου
+8003	8003	HEDNO_epaggelmatikoi_8003	Καβάλας
+8004	8004	HEDNO_epaggelmatikoi_8004	Ν.Ιωνιας
+8005	8005	HEDNO_epaggelmatikoi_8005	Βόλου
+8006	8006	HEDNO_epaggelmatikoi_8006	Δυτ. Θεσ/κης
+8007	8007	HEDNO_epaggelmatikoi_8007	Κορωπίου
+8008	8008	HEDNO_epaggelmatikoi_8008	Κορωπίου
+8009	8009	HEDNO_epaggelmatikoi_8009	Κορωπίου
+8010	8010	HEDNO_epaggelmatikoi_8010	Πάτρας
+8011	8011	HEDNO_epaggelmatikoi_8011	Πάτρας
+8012	8012	HEDNO_epaggelmatikoi_8012	Δυτ. Θεσ/κης
+8013	8013	HEDNO_epaggelmatikoi_8013	Ανατ. Θεσ/κης
+8014	8014	HEDNO_epaggelmatikoi_8014	Φλώρινας
+8015	8015	HEDNO_epaggelmatikoi_8015	Καλλιθέας
+8016	8016	HEDNO_epaggelmatikoi_8016	Αιγάλεω
+8017	8017	HEDNO_epaggelmatikoi_8017	Ανατ. Θεσ/κης
+8018	8018	HEDNO_epaggelmatikoi_8018	Κορωπίου
+8019	8019	HEDNO_epaggelmatikoi_8019	Ανατ. Θεσ/κης
+8020	8020	HEDNO_epaggelmatikoi_8020	Πάτρας
+8021	8021	HEDNO_epaggelmatikoi_8021	Πάτρας
+8022	8022	HEDNO_epaggelmatikoi_8022	Πάτρας
+8023	8023	HEDNO_epaggelmatikoi_8023	Κ.Αχαϊας
+8024	8024	HEDNO_epaggelmatikoi_8024	Βόλου
+8025	8025	HEDNO_epaggelmatikoi_8025	Αγρινίου
+8026	8026	HEDNO_epaggelmatikoi_8026	Καβάλας
+8027	8027	HEDNO_epaggelmatikoi_8027	Καβάλας
+8028	8028	HEDNO_epaggelmatikoi_8028	Γλυφάδας
+8029	8029	HEDNO_epaggelmatikoi_8029	Δυτ. Θεσ/κης
+8030	8030	HEDNO_epaggelmatikoi_8030	Ανατ. Θεσ/κης
+8031	8031	HEDNO_epaggelmatikoi_8031	Πάτρας
+8032	8032	HEDNO_epaggelmatikoi_8032	Δυτ. Θεσ/κης
+8033	8033	HEDNO_epaggelmatikoi_8033	Κορωπίου
+8034	8034	HEDNO_epaggelmatikoi_8034	Καλλιθέας
+8035	8035	HEDNO_epaggelmatikoi_8035	Αιγάλεω
+8036	8036	HEDNO_epaggelmatikoi_8036	Πάτρας
+8037	8037	HEDNO_epaggelmatikoi_8037	Λάρισας
+8038	8038	HEDNO_epaggelmatikoi_8038	Δυτ. Θεσ/κης
+8039	8039	HEDNO_epaggelmatikoi_8039	Δυτ. Θεσ/κης
+8040	8040	HEDNO_epaggelmatikoi_8040	Αιγίου
+8041	8041	HEDNO_epaggelmatikoi_8041	Βόλου
+8042	8042	HEDNO_epaggelmatikoi_8042	Φλώρινας
+8043	8043	HEDNO_epaggelmatikoi_8043	Φλώρινας
+8044	8044	HEDNO_epaggelmatikoi_8044	Λάρισας
+8045	8045	HEDNO_epaggelmatikoi_8045	Κορωπίου
+8046	8046	HEDNO_epaggelmatikoi_8046	Κορωπίου
+8047	8047	HEDNO_epaggelmatikoi_8047	Βόλου
+8048	8048	HEDNO_epaggelmatikoi_8048	Αιγίου
+8049	8049	HEDNO_epaggelmatikoi_8049	Ανατ. Θεσ/κης
+8050	8050	HEDNO_epaggelmatikoi_8050	Αιγίου
