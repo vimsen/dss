7788fe4d1c24286316ac44115d9b4035031a118d
Dimitrios J. Vergados <djvergad@gmail.com>
2016-09-23 15:11:23 +0300

    Change prosumer name to include HEDNO prefix
    Former-commit-id: ed398a6c024bc6f071a50bcda793f2a478b97eba

--- test/fixtures/prosumers_biomhxanikoi_MV.sql
+++ test/fixtures/prosumers_biomhxanikoi_MV.sql
@@ -1,50 +1,50 @@
-7001	107001	biomhxanikoi_MV_7001	Μ.Τ Περ. Περιστερίου
-7002	107002	biomhxanikoi_MV_7002	Δυτ. Θεσ/κης
-7003	107003	biomhxanikoi_MV_7003	Κιλκίς
-7004	107004	biomhxanikoi_MV_7004	Βόλου
-7005	107005	biomhxanikoi_MV_7005	Βόλου
-7006	107006	biomhxanikoi_MV_7006	Βόλου
-7007	107007	biomhxanikoi_MV_7007	Βόλου
-7008	107008	biomhxanikoi_MV_7008	Δυτ. Θεσ/κης
-7009	107009	biomhxanikoi_MV_7009	Κιλκίς
-7010	107010	biomhxanikoi_MV_7010	Δυτ. Θεσ/κης
-7011	107011	biomhxanikoi_MV_7011	Δυτ. Θεσ/κης
-7012	107012	biomhxanikoi_MV_7012	Δυτ. Θεσ/κης
-7013	107013	biomhxanikoi_MV_7013	Κιλκίς
-7014	107014	biomhxanikoi_MV_7014	Κιλκίς
-7015	107015	biomhxanikoi_MV_7015	Κιλκίς
-7016	107016	biomhxanikoi_MV_7016	Κιλκίς
-7017	107017	biomhxanikoi_MV_7017	Κιλκίς
-7018	107018	biomhxanikoi_MV_7018	Κιλκίς
-7019	107019	biomhxanikoi_MV_7019	Κιλκίς
-7020	107020	biomhxanikoi_MV_7020	Κεντρ. Θεσ/κης
-7021	107021	biomhxanikoi_MV_7021	Δυτ. Θεσ/κης
-7022	107022	biomhxanikoi_MV_7022	Αρναίας
-7023	107023	biomhxanikoi_MV_7023	Κιλκίς
-7024	107024	biomhxanikoi_MV_7024	Βόλου
-7025	107025	biomhxanikoi_MV_7025	Μ.Τ Περ. Περιστερίου
-7026	107026	biomhxanikoi_MV_7026	Βόλου
-7027	107027	biomhxanikoi_MV_7027	Βόλου
-7028	107028	biomhxanikoi_MV_7028	Βόλου
-7029	107029	biomhxanikoi_MV_7029	Βόλου
-7030	107030	biomhxanikoi_MV_7030	Πάτρας
-7031	107031	biomhxanikoi_MV_7031	Πάτρας
-7032	107032	biomhxanikoi_MV_7032	Πάτρας
-7033	107033	biomhxanikoi_MV_7033	Πάτρας
-7034	107034	biomhxanikoi_MV_7034	Πάτρας
-7035	107035	biomhxanikoi_MV_7035	Πάτρας
-7036	107036	biomhxanikoi_MV_7036	Πάτρας
-7037	107037	biomhxanikoi_MV_7037	Πάτρας
-7038	107038	biomhxanikoi_MV_7038	Πάτρας
-7039	107039	biomhxanikoi_MV_7039	Πάτρας
-7040	107040	biomhxanikoi_MV_7040	Βόλου
-7041	107041	biomhxanikoi_MV_7041	Μ.Τ Περ. Περιστερίου
-7042	107042	biomhxanikoi_MV_7042	Μ.Τ Περ. Περιστερίου
-7043	107043	biomhxanikoi_MV_7043	Πάτρας
-7044	107044	biomhxanikoi_MV_7044	Κεντρ. Θεσ/κης
-7045	107045	biomhxanikoi_MV_7045	Δυτ. Θεσ/κης
-7046	107046	biomhxanikoi_MV_7046	Βόλου
-7047	107047	biomhxanikoi_MV_7047	Βόλου
-7048	107048	biomhxanikoi_MV_7048	Κιλκίς
-7049	107049	biomhxanikoi_MV_7049	Πάτρας
-7050	107050	biomhxanikoi_MV_7050	Δυτ. Θεσ/κης
+7001	7001	HEDNO_biomhxanikoi_MV_7001	Μ.Τ Περ. Περιστερίου
+7002	7002	HEDNO_biomhxanikoi_MV_7002	Δυτ. Θεσ/κης
+7003	7003	HEDNO_biomhxanikoi_MV_7003	Κιλκίς
+7004	7004	HEDNO_biomhxanikoi_MV_7004	Βόλου
+7005	7005	HEDNO_biomhxanikoi_MV_7005	Βόλου
+7006	7006	HEDNO_biomhxanikoi_MV_7006	Βόλου
+7007	7007	HEDNO_biomhxanikoi_MV_7007	Βόλου
+7008	7008	HEDNO_biomhxanikoi_MV_7008	Δυτ. Θεσ/κης
+7009	7009	HEDNO_biomhxanikoi_MV_7009	Κιλκίς
+7010	7010	HEDNO_biomhxanikoi_MV_7010	Δυτ. Θεσ/κης
+7011	7011	HEDNO_biomhxanikoi_MV_7011	Δυτ. Θεσ/κης
+7012	7012	HEDNO_biomhxanikoi_MV_7012	Δυτ. Θεσ/κης
+7013	7013	HEDNO_biomhxanikoi_MV_7013	Κιλκίς
+7014	7014	HEDNO_biomhxanikoi_MV_7014	Κιλκίς
+7015	7015	HEDNO_biomhxanikoi_MV_7015	Κιλκίς
+7016	7016	HEDNO_biomhxanikoi_MV_7016	Κιλκίς
+7017	7017	HEDNO_biomhxanikoi_MV_7017	Κιλκίς
+7018	7018	HEDNO_biomhxanikoi_MV_7018	Κιλκίς
+7019	7019	HEDNO_biomhxanikoi_MV_7019	Κιλκίς
+7020	7020	HEDNO_biomhxanikoi_MV_7020	Κεντρ. Θεσ/κης
+7021	7021	HEDNO_biomhxanikoi_MV_7021	Δυτ. Θεσ/κης
+7022	7022	HEDNO_biomhxanikoi_MV_7022	Αρναίας
+7023	7023	HEDNO_biomhxanikoi_MV_7023	Κιλκίς
+7024	7024	HEDNO_biomhxanikoi_MV_7024	Βόλου
+7025	7025	HEDNO_biomhxanikoi_MV_7025	Μ.Τ Περ. Περιστερίου
+7026	7026	HEDNO_biomhxanikoi_MV_7026	Βόλου
+7027	7027	HEDNO_biomhxanikoi_MV_7027	Βόλου
+7028	7028	HEDNO_biomhxanikoi_MV_7028	Βόλου
+7029	7029	HEDNO_biomhxanikoi_MV_7029	Βόλου
+7030	7030	HEDNO_biomhxanikoi_MV_7030	Πάτρας
+7031	7031	HEDNO_biomhxanikoi_MV_7031	Πάτρας
+7032	7032	HEDNO_biomhxanikoi_MV_7032	Πάτρας
+7033	7033	HEDNO_biomhxanikoi_MV_7033	Πάτρας
+7034	7034	HEDNO_biomhxanikoi_MV_7034	Πάτρας
+7035	7035	HEDNO_biomhxanikoi_MV_7035	Πάτρας
+7036	7036	HEDNO_biomhxanikoi_MV_7036	Πάτρας
+7037	7037	HEDNO_biomhxanikoi_MV_7037	Πάτρας
+7038	7038	HEDNO_biomhxanikoi_MV_7038	Πάτρας
+7039	7039	HEDNO_biomhxanikoi_MV_7039	Πάτρας
+7040	7040	HEDNO_biomhxanikoi_MV_7040	Βόλου
+7041	7041	HEDNO_biomhxanikoi_MV_7041	Μ.Τ Περ. Περιστερίου
+7042	7042	HEDNO_biomhxanikoi_MV_7042	Μ.Τ Περ. Περιστερίου
+7043	7043	HEDNO_biomhxanikoi_MV_7043	Πάτρας
+7044	7044	HEDNO_biomhxanikoi_MV_7044	Κεντρ. Θεσ/κης
+7045	7045	HEDNO_biomhxanikoi_MV_7045	Δυτ. Θεσ/κης
+7046	7046	HEDNO_biomhxanikoi_MV_7046	Βόλου
+7047	7047	HEDNO_biomhxanikoi_MV_7047	Βόλου
+7048	7048	HEDNO_biomhxanikoi_MV_7048	Κιλκίς
+7049	7049	HEDNO_biomhxanikoi_MV_7049	Πάτρας
+7050	7050	HEDNO_biomhxanikoi_MV_7050	Δυτ. Θεσ/κης
