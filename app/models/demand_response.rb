ed1d4c7b1a98bb5541b688e19647e85825d4dace
Dimitrios J. Vergados <djvergad@gmail.com>
2016-11-29 16:26:47 +0200

    after_commit caused a loop
    Former-commit-id: fdfc337eb040350aa43881389db34f22c5f3e539 [formerly fdfc337eb040350aa43881389db34f22c5f3e539 [formerly ec97d98aa06285f6aed9c87ebc518b6c6aef6de9]]
    Former-commit-id: f543e5ff5df9e073bc2818df592253d4dfe32d05
    Former-commit-id: 840a26f06e237c25c281e75055cc5b5fde4368a3

--- app/models/demand_response.rb
+++ app/models/demand_response.rb
@@ -13,7 +13,7 @@ class DemandResponse < ActiveRecord::Base
   has_many :dr_planneds, dependent: :destroy
   has_many :dr_actuals, dependent: :destroy
 
-  after_commit do
+  after_create do
     agent = FetchAsynch::DemandResponseAgent.new
     agent.dr_activation self.reload, self.feeder_id, self.prosumer_category
   end
