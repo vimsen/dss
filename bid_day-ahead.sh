2ab5989c3438ea7a80ee2dbb0295bc2a8320ed61
Dimitrios J. Vergados <djvergad@gmail.com>
2016-12-08 15:06:53 +0200

    Try a different date
    Former-commit-id: 56c1cf59374d161d734c6cfdb9e2e12c6d04e488 [formerly 8fd8bc92b3b24b4400b17edb48e7d54ebf6c39d0 [formerly 247a24bd170bb65d7423512ad7e1efd6739cc8f8] [formerly 247a24bd170bb65d7423512ad7e1efd6739cc8f8 [formerly 560f58062a33372be08bac1ee7a26f5e74789778]]]
    Former-commit-id: 1ef0e19e39356067fa49acdeed18d086d7e5d6f9
    Former-commit-id: 1aee86811bc55145bae6b4d281f2cfd0e27c57fa
    Former-commit-id: e421e1272559a17615e0a276e3f06b799653b69f

--- bid_day-ahead.sh
+++ bid_day-ahead.sh
@@ -2,6 +2,6 @@
 rails runner -e development '
         BidDayAheadJob.perform_now(
                 prosumers: Prosumer.where(prosumer_category_id: 4).order(edms_id: :asc),
-                date: "21/11/2015".to_date
+                date: "18/11/2015".to_date
 #                starttime: "2015-11-09 11:45:00 +0200".to_datetime
          )'
