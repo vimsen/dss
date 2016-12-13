63054da67402d39cee0a33acf02b5d8db9736b0c
Dimitrios J. Vergados <djvergad@gmail.com>
2016-12-13 17:01:31 +0200

    Added inraday typeto model
    Former-commit-id: 9222fecc8bb6dbc47c695f37ef71f3df3e8873ad [formerly 166ac3c5eeb67e6e86ed0c726af2edcba338b776 [formerly 55cc51546831b599eada4c8f669bb1e32ae7cfb3] [formerly 55cc51546831b599eada4c8f669bb1e32ae7cfb3 [formerly 45018773726cb271081f5fe348d44e6ddceee9ad]]]
    Former-commit-id: 082c020b47eb22cf905904a1513a7562e3e1f4b7
    Former-commit-id: 2e1b912207cbb3a42b0dbf450d43c84feb1cb3fb
    Former-commit-id: 38290b6041a3d6bde93f0c9fbdfe3af9f58bb3f8

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -2,5 +2,5 @@ class Forecast < ActiveRecord::Base
   belongs_to :prosumer
   belongs_to :interval
 
-  enum forecast_type: [ :day_ahead, :real_time ]
+  enum forecast_type: [ :day_ahead, :inra_day, :real_time ]
 end
